20702c286e369708d7e7e3d9eb041231bfac648e
Shoejust <141555665+JustinHsu1019@users.noreply.github.com>
2023-10-17 11:26:21 +0800

    Update lesson_example.sql

--- MySQL/lesson_example.sql
+++ MySQL/lesson_example.sql
@@ -1,81 +1,71 @@
+-- 主題：學生資料管理系統
+
 -- Section 1: CREATE and ALTER Statements
 
--- 1. Create a new database
+-- 建立新的資料庫
 CREATE DATABASE schoolDB;
-USE schoolDB; -- Switch to the database
 
--- 2. Create new tables
+-- 切換到該資料庫
+USE schoolDB;
+
+-- 建立科目資料表
 CREATE TABLE subjects (
     id INT AUTO_INCREMENT PRIMARY KEY,
     subject_name VARCHAR(50) NOT NULL
 );
 
+-- 建立學生資料表
 CREATE TABLE students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     subject_id INT,
     grade DECIMAL(4,2),
+    email VARCHAR(100),
     FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
 
--- 3. Modify the table structure
+-- 修改學生資料表
 ALTER TABLE students ADD COLUMN email VARCHAR(100);
 
 -- Section 2: Basic Query Statements
 
--- Inserting some sample data for demo purposes
+-- 插入科目資料
 INSERT INTO subjects (subject_name) VALUES ('Mathematics'), ('History'), ('Physics');
 
+-- 插入學生資料
 INSERT INTO students (first_name, last_name, subject_id, grade, email)
 VALUES ('John', 'Doe', 1, 88.5, 'johndoe@gmail.com'),
        ('Jane', 'Smith', 2, 90.0, 'janesmith@gmail.com'),
        ('Robert', 'Johnson', 3, 85.5, 'robertjohnson@gmail.com');
 
--- 2. Update existing data
+-- 更新學生成績
 UPDATE students SET grade = 89.5 WHERE id = 3;
 
--- 1. Basic SELECT statements
+-- 基本查詢語句
 SELECT * FROM students;
 SELECT first_name, last_name FROM students;
 
--- 2. Using WHERE clause to filter data
-SELECT * FROM students WHERE grade > 85;
+-- 使用 WHERE 子句來過濾資料
+SELECT first_name, last_name FROM students WHERE grade > 85;
 SELECT first_name, last_name FROM students WHERE subject_id = 2;
 
--- 3. Sorting and Grouping data
+-- 資料的排序和分組
 SELECT subject_id, COUNT(*) as num_students FROM students GROUP BY subject_id;
 SELECT first_name, last_name FROM students ORDER BY last_name DESC;
 
 -- Section 3: Advanced Query Techniques
 
--- 1. Views
+-- 建立View
 CREATE VIEW view_students AS SELECT first_name, last_name, email FROM students WHERE subject_id = 2;
 SELECT * FROM view_students;
 
--- 2. JOINs
--- Note: For a meaningful JOIN, we need another table.
-CREATE TABLE clubs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    club_name VARCHAR(50)
-);
-
-CREATE TABLE student_clubs (
-    student_id INT,
-    club_id INT,
-    FOREIGN KEY (student_id) REFERENCES students(id),
-    FOREIGN KEY (club_id) REFERENCES clubs(id)
-);
-
-INSERT INTO clubs (club_name) VALUES ('Chess Club'), ('Science Club'), ('Debate Club');
-INSERT INTO student_clubs (student_id, club_id) VALUES (1, 1), (2, 3), (3, 2);
-
--- Examples for JOIN
-SELECT s.first_name, c.club_name 
+-- JOINs
+-- 透過學生和科目資料表的JOIN，查詢學生的名字和他們所修的科目
+SELECT s.first_name, s.last_name, sub.subject_name 
 FROM students s
-INNER JOIN student_clubs sc ON s.id = sc.student_id
-INNER JOIN clubs c ON sc.club_id = c.id;
+INNER JOIN subjects sub ON s.subject_id = sub.id;
 
--- 3. Sub-queries
+-- 子查詢
 SELECT first_name FROM students WHERE subject_id IN (SELECT id FROM subjects WHERE subject_name = 'History');
 SELECT first_name FROM students WHERE grade > (SELECT AVG(grade) FROM students);
